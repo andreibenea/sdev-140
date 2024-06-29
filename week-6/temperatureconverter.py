908da3cb2b0d4856b8b8af60e43c78f68a85e636
Andrei Benea <andrei.benea89@gmail.com>
2024-06-29 17:04:51 -0400

    add breezy imports

---
+++ week-6/temperatureconverter.py
@@ -0,0 +1,15 @@
+"""
+Author: Andrei Benea
+Date written: 6/29/24
+Assignment: M06 Practice Programming Exercise 9-5
+This program uses a GUI to allow the user to type in a temperature value
+inside an input field. This value can represent either degrees Celsius or 
+degrees Fahrenheit. Two additional buttons will allow the user to trigger
+the conversion of the previously typed in value in the desired counterpart:
+degrees C or F.
+The program uses the following formula to make the conversion:
+F = 9/5 * C + 32
+F is the Fahrenheit temperature, and C is the Celsius temperature.
+"""
+
+from breezypythongui import EasyFrame
