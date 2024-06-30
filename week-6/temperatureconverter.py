9d02ed413fbac3947bbca85061b3c8bb6260b5d6
Andrei Benea <andrei.benea89@gmail.com>
2024-06-30 16:24:15 -0400

    add minor disable/restore commands to buttons

--- week-6/temperatureconverter.py
+++ week-6/temperatureconverter.py
@@ -25,8 +25,12 @@ class TemperatureConverter(EasyFrame):
         self.addFloatField(0, row=1, column=1, precision=2, sticky="NSEW").setNumber(
             0.00
         )
-        self.addButton(text="Celsius", row=1, column=0)
-        self.addButton(text="Fahrenheit", row=1, column=2)
+        self.celsiusButton = self.addButton(
+            text="Celsius", row=1, column=0, command=self.convert_to_celsius
+        )
+        self.fahrenheitButton = self.addButton(
+            text="Fahrenheit", row=1, column=2, command=self.convert_to_fahrenheit
+        )
         self.addLabel(
             text="Type in value above, then click either buttons to convert",
             row=2,
@@ -36,6 +40,14 @@ class TemperatureConverter(EasyFrame):
         imageLabel = self.addLabel(text="", row=0, rowspan=3, column=3, sticky="NSEW")
         self.image = PhotoImage(file="week-6/thermometer.gif")
         imageLabel["image"] = self.image
+
+    def convert_to_celsius(self):
+        self.celsiusButton["state"] = "disabled"
+        self.fahrenheitButton["state"] = "normal"
+
+    def convert_to_fahrenheit(self):
+        self.celsiusButton["state"] = "normal"
+        self.fahrenheitButton["state"] = "disabled"
 
 
 def main():
