75b14bdbbd8f2058fb798dd7ef76179b3be74d8d
Andrei Benea <andrei.benea89@gmail.com>
2024-07-01 12:32:19 -0400

    add guessinggame file w. description

---
+++ week-6/guessinggame.py
@@ -0,0 +1,11 @@
+"""
+Author: Andrei Benea
+Date written: 7/1/24
+Assignment: M06 Programming Assignment
+This program is a game in which the computer is the one guessing
+a number, while the user is responding with either "Too small",
+"Too large", or "Correct". Depending on the user's response, the
+computer will generate a new number (trying to guess correctly),
+or will consider the game over. The game starts (again) using a
+"New Game" button.
+"""
