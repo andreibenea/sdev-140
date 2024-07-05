2a1c9199ed5b561215537f1d95911b17ac50cdda
Andrei Benea <andrei.benea89@gmail.com>
2024-07-04 20:24:20 -0400

    add more btns, disable/enable func

--- week-6/guessinggame.py
+++ week-6/guessinggame.py
@@ -12,9 +12,14 @@ or will consider the game over. The game starts (again) using a
 
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 import random
 
-root = Tk()
+
+# print(tk.TkVersion)
+# print(tk.TclVersion)
+
+root = tk.Tk()
 frm_play_area = ttk.Frame(root, padding=10)
 frm_buttons = ttk.Frame(root, padding=10)
 frm_play_area.grid()
@@ -27,12 +32,26 @@ def generateNumber():
     updateDisplay(result)
 
 
+def numberTooLow():
+    pass
+
+
+def numberTooHigh():
+    pass
+
+
+def exactMatch():
+    new_game_btn["state"] = "normal"
+    pass
+
+
 def updateDisplay(string_value):
-    displayVariable.set(string_value)
+    displayVariable.set(f"Is {string_value} the number you had in mind?")
 
 
 def playGame():
-    print(generateNumber())
+    new_game_btn["state"] = "disabled"
+    generateNumber()
 
 
 def main():
@@ -41,18 +60,33 @@ def main():
 
 
 displayVariable = StringVar()
-ttk.Label(frm_play_area, text="Hello Friend!").grid(column=0, row=0)
+
+greeting_area = ttk.Label(frm_play_area, text="Hello Friend!").grid(column=0, row=0)
 play_area = ttk.Label(
     frm_play_area,
     text="Think of a number between 1 and 500 then click 'New Game' to start the guessing game!",
+)
+play_area.grid(column=0, row=1)
+number_area = ttk.Label(
+    frm_play_area,
     textvariable=displayVariable,
-).grid(column=0, row=1)
-new_game_btn = ttk.Button(frm_buttons, text="New Game", command=playGame).grid(
-    column=0, row=0
 )
-quit_game_btn = ttk.Button(frm_buttons, text="Quit", command=root.destroy).grid(
-    column=2, row=0
+number_area.grid(column=0, row=2)
+
+new_game_btn = ttk.Button(frm_buttons, text="New Game", command=lambda: [playGame()])
+new_game_btn.grid(column=0, row=0)
+too_low_btn = ttk.Button(frm_buttons, text="Too low!", command=lambda: [numberTooLow()])
+too_low_btn.grid(column=1, row=0)
+too_high_btn = ttk.Button(
+    frm_buttons, text="Too high!", command=lambda: [numberTooHigh()]
 )
+too_high_btn.grid(column=2, row=0)
+exact_match_btn = ttk.Button(
+    frm_buttons, text="That's correct!", command=lambda: [exactMatch()]
+)
+exact_match_btn.grid(column=3, row=0)
+quit_game_btn = ttk.Button(frm_buttons, text="Quit", command=root.destroy)
+quit_game_btn.grid(column=4, row=0)
 
 
 if __name__ == "__main__":
