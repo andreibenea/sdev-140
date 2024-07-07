43b384fb9e2251f1cc93c30e4b65550f8d5de4ef
Andrei Benea <andrei.benea89@gmail.com>
2024-07-07 12:54:04 -0400

    add comments, update date written

--- week-7/BeneaAndreiM07_M07_Ex1.py
+++ week-7/BeneaAndreiM07_M07_Ex1.py
@@ -1,11 +1,12 @@
 """
 Author: Andrei Benea
-Date written: 7/6/24
+Date written: 7/7/24
 Assignment: M07 Programming Assignment Part 1
 This program is an example of using a class and sub-class.
 """
 
 
+# create person class
 class Person:
     def __init__(self, name, address, phone_number) -> None:
         self.name = name
@@ -32,6 +33,7 @@ class Person:
         )
 
 
+# create customer sub-class
 class Customer(Person):
     def __init__(
         self, name, address, phone_number, customer_number, wants_mailing_list
@@ -56,8 +58,10 @@ class Customer(Person):
         )
 
 
+# create object based on customer class
 person_one = Customer("John Doe", "New York, NY", "(123) 456 7890", 1, True)
 
-
+# print object method
 print(person_one.getName())
+# print object
 print(person_one)
