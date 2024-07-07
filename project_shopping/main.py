03fa9c3388c5fd8b823b007da93d74701612ea8c
Andrei Benea <andrei.benea89@gmail.com>
2024-07-07 13:12:34 -0400

    update to ttk structure

--- project_shopping/main.py
+++ project_shopping/main.py
@@ -1,9 +1,25 @@
+# import modules
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
+# create widgets
+root = tk.Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
+
+
+# define functions
+def main():
+    root.mainloop()
+
+
+# create labels
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+
+# create buttons
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+
+# call main
+if __name__ == "__main__":
+    main()
