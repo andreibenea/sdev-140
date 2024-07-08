479d1ae1e7fc73d5e51d4ce3ab0fc0ce135d2805
Andrei Benea <andrei.benea89@gmail.com>
2024-07-08 12:07:19 -0400

    created main page unrefined

--- project_shopping/main.py
+++ project_shopping/main.py
@@ -15,20 +15,64 @@ if needed.
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from tkinter.font import Font
 
 
 # create main page
 class MainPage(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        self.set_fonts()
         self.create_widgets()
+        
+    def set_fonts(self):
+        self.titleFont = Font(weight="bold", size=22)
 
     def create_widgets(self):
-        self.label = ttk.Label(self, text="Hello World")
-        self.label.grid(column=0, row=0)
+        self.mainFrame = ttk.Frame(self, padding=10)
+        self.mainFrame.grid()
 
-        self.button = ttk.Button(self, text="Quit", command=self.quitApplication)
-        self.button.grid(column=1, row=1)
+        self.menuButton = tk.Menubutton(self.mainFrame, text="Menu", relief=RAISED, width=3)
+        self.menuButton.grid(row=0, column=0)
+        self.menuButton.menu = tk.Menu(self.menuButton, tearoff=0)
+        self.menuButton["menu"] = self.menuButton.menu
+
+        self.activeList = tk.IntVar()
+        self.allLists = tk.IntVar()
+        self.menuButton.menu.add_checkbutton(
+            label="Active List", variable=self.activeList
+        )
+        self.menuButton.menu.add_checkbutton(label="All Lists", variable=self.allLists)
+
+        self.pageTitle = ttk.Label(self.mainFrame, text="Shopping Manager")
+        self.pageTitle.grid(row=1, column=1)
+        self.pageTitle["font"] = self.titleFont
+
+        self.button = ttk.Button(
+            self.mainFrame, text="Quit", command=self.quitApplication, width=3
+        )
+        self.button.grid(row=0, column=2)
+
+        self.searchBar = ttk.Entry(self.mainFrame)
+        self.searchBar.grid(row=2, column=1, sticky="NWES")
+
+        self.addToCartImage = tk.PhotoImage(
+            file="project_shopping/icons/icons8-cart-16.png"
+        )
+        self.addButton = ttk.Button(
+            self.mainFrame,
+            text="Add",
+            width=2.5,
+            image=self.addToCartImage,
+            compound=RIGHT,
+        )
+        self.addButton.grid(row=2, column=1, sticky="NES")
+
+        self.allListsImage = tk.PhotoImage(
+            file="project_shopping/icons/icons8-task-lists-100.png"
+        )
+        self.allListsImageLabel = ttk.Label(self.mainFrame, image=self.allListsImage)
+        self.allListsImageLabel.grid(row=3, column=1)
 
     def quitApplication(self):
         self.master.quit()
